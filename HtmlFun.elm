a6a64fe317638fa1af8954bce8ae9d2c5be7c639
Joshua Book <joshua.book@gmail.com>
2017-08-07 14:48:45 -0400

    start some html fun

---
+++ HtmlFun.elm
@@ -0,0 +1,24 @@
+module Main exposing (..)
+
+import Html exposing(..)
+import Html.Attributes exposing(..)
+
+box =
+  div
+  [ style
+    [ ("height", "400px")
+    , ("width", "400px")
+    , ("border", "1px solid black")
+    ]
+  ]
+  [
+    h1
+      [
+      ]
+      [
+        text "hello div"
+      ]
+  ]
+
+main =
+  box
