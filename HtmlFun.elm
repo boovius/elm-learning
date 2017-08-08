e84a730552fe94bfd17c3d663705dd120ade32f2
Joshua Book <joshua.book@gmail.com>
2017-08-08 09:58:21 -0400

    formatting

--- HtmlFun.elm
+++ HtmlFun.elm
@@ -1,24 +1,23 @@
 module Main exposing (..)
 
-import Html exposing(..)
-import Html.Attributes exposing(..)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
 
 box =
-  div
-  [ style
-    [ ("height", "400px")
-    , ("width", "400px")
-    , ("border", "1px solid black")
-    ]
-  ]
-  [
-    h1
-      [
-      ]
-      [
-        text "hello div"
-      ]
-  ]
+    div
+        [ style
+            [ ( "height", "400px" )
+            , ( "width", "400px" )
+            , ( "border", "1px solid black" )
+            ]
+        ]
+        [ h1
+            []
+            [ text "hello div"
+            ]
+        ]
+
 
 main =
-  box
+    box
